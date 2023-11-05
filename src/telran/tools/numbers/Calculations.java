81336766d949dafae298d11a54a803e9140a516c
A M Mokades <akinshinmik@gmail.com>
2023-11-05 20:31:19 +0200

    Initial code

---
+++ src/telran/tools/numbers/Calculations.java
@@ -0,0 +1,18 @@
+package telran.tools.numbers;
+
+public class Calculations {
+	public static double sum(double op1, double op2) {
+		return op1 + op2;
+		
+	}
+	public static double substract(double op1, double op2) {
+		return op1 - op2;
+	}
+	public static double multiply(double op1, double op2) {
+		return op1 * op2;
+	}
+	public static double divide(double op1,double op2) {
+		return op1/op2;
+	}
+
+}
